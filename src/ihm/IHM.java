16b069ac11ed06c84008801327a17fd2cde19277
unknown <nicolasguy1@free.fr>
2018-05-21 14:40:39 +0200

    IHM sans fonctionnalitées

---
+++ src/ihm/IHM.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ihm;
+
+import java.io.IOException;
+
+
+/**
+ *
+ * @author Nico
+ */
+public class IHM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+       // new Accueil().setVisible(true);
+        new clientTCP();
+    }
+    
+}
---
+++ src/ihm/clientTCP.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ihm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ *
+ * @author Nico
+ */
+public class clientTCP {
+
+    private boolean etatClient;
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+
+    public clientTCP() throws IOException {
+        etatClient = false;
+        this.demarrageClient();
+    }
+
+    public void demarrageClient() throws IOException {
+
+       
+            socket = new Socket("192.168.1.83", 1234);
+            System.out.println("Connexion effectuée");
+
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream());
+            etatClient = true;
+    }
+
+    public void fermerClient() throws IOException {
+        socket.close();
+        etatClient = false;
+    }
+
+}
